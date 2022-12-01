04bf0370c0932c86fab89e2dec7bf26020b8e718
Roman Buldygin <froooks@gmail.com>
2022-12-01 03:14:51 +0300

    publish works

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -6,10 +7,12 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
@@ -19,26 +22,53 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public Build()
+    {
+        OutputPath = RootDirectory / "out";
+        PublishPath = OutputPath / "publish";
+        DebBuildPath = OutputPath / "deb";
+    }
+
+    public static int Main () => Execute<Build>(x => x.Publish);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    readonly AbsolutePath OutputPath;
+    readonly AbsolutePath PublishPath;
+    readonly AbsolutePath DebBuildPath;
+
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+    Project PublishProject => Solution.GetProject("OpenHdWebUi.Server");
+
+    IReadOnlyCollection<string> Rims => PublishProject.GetRuntimeIdentifiers();
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            EnsureCleanDirectory(OutputPath);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(settings => settings
+                .SetProjectFile(Solution));
         });
 
-    Target Compile => _ => _
+    Target Publish => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetPublish(settings => settings
+                .SetNoRestore(true)
+                .SetProject(PublishProject)
+                .SetConfiguration(Configuration)
+                .SetSelfContained(true)
+                .CombineWith(Rims, (_, s) => _
+                    .SetOutput(PublishPath / s)
+                    .SetRuntime(s)));
         });
-
 }
