bc6d2f06a858173c06510f88ffe6ed46dc96ea9e
Roman Buldygin <froooks@gmail.com>
2023-02-18 02:39:14 +0300

    WebRTC demo

---
+++ src/RtpToWebRestreamerDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenHdWebUi.RtpToWebRestreamer;
+
+Console.WriteLine("Hello, World!");
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(builder => builder
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Trace))
+    .Build();
+
+var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+
+var handler = new RtpRestreamer(
+    new IPEndPoint(IPAddress.Any, 8081),
+    new IPEndPoint(IPAddress.Any, 5600),
+    loggerFactory);
+handler.Start();
+
+host.Run();
