d73ff372e9559031663d68b09c24fbcbfeaf4e57
Roman Buldygin <froooks@gmail.com>
2023-02-22 00:44:36 +0300

    Save

--- src/OpenHdWebUi.Server/ClientApp/src/app/fpv/fpv.component.ts
+++ src/OpenHdWebUi.Server/ClientApp/src/app/fpv/fpv.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
-import {SignalrService} from "../signalr.service"
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Inject } from '@angular/core';
+import { SignalrService } from "../signalr.service"
 
 @Component({
   selector: 'app-fpv',
@@ -9,25 +9,25 @@ import {SignalrService} from "../signalr.service"
 export class FpvComponent implements OnInit, AfterViewInit {
   @ViewChild("fpvVideo") video?: ElementRef<HTMLVideoElement>;
 
+  private baseUrl: string;
   private pc?: RTCPeerConnection;
   private ws?: WebSocket;
 
   public demoText = "tests";
 
 
-  constructor() {
-
+  constructor(@Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit() {
-    this.start();
+    this.start()
+      .then(() => console.log("Started") )
+      .catch(reason => console.log("Error",reason));
   }
-
-
-
   async start() {
     if (this.ws != null) await this.ws.close();
     if (this.pc != null) await this.pc.close();
@@ -44,7 +44,8 @@ export class FpvComponent implements OnInit, AfterViewInit {
     this.pc.onsignalingstatechange = () => console.log("onsignalingstatechange: " + this.pc!.signalingState);
     this.pc.onconnectionstatechange = () => console.log("onconnectionstatechange: " + this.pc!.connectionState);
 
-    this.ws = new WebSocket("ws://127.0.0.1:8081/", []);
+    let url = new URL(this.baseUrl);
+    this.ws = new WebSocket(`ws://${ url.host }:8081/`, []);
 
     let _this = this;
     this.ws.onmessage = async function (evt) {
