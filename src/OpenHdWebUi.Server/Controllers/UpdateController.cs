6b115d94f283654df1def479c2ab2c79ecbf7fc5
Roman Buldygin <froooks@gmail.com>
2025-02-12 00:43:44 +0300

    New controller

---
+++ src/OpenHdWebUi.Server/Controllers/UpdateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenHdWebUi.Server.Controllers;
+
+[Route("api/update")]
+[ApiController]
+public class UpdateController: ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<ActionResult> UploadFile()
+    {
+        var requestStream = Request.Body;
+        await using var fileStream = System.IO.File.Create("update.zip");
+        await requestStream.CopyToAsync(fileStream);
+
+        return NoContent();
+    }
+}
