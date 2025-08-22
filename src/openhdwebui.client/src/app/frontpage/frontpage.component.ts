b750b7d22c8ffa8953f3d49a90da105d9c0ad402
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-08-22 21:28:25 +0200

    look and feel

--- src/openhdwebui.client/src/app/frontpage/frontpage.component.ts
+++ src/openhdwebui.client/src/app/frontpage/frontpage.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-frontpage',
   templateUrl: './frontpage.component.html',
-  styleUrls: ['./frontpage.component.css']
+  styleUrls: ['./frontpage.component.scss']
 })
-export class FrontpageComponent { }
+export class FrontpageComponent {
+  isLoginOpen = false;
+
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    remember: [true]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  openLogin() { this.isLoginOpen = true; }
+  closeLogin() { this.isLoginOpen = false; }
+
+  submitLogin() {
+    if (this.loginForm.invalid) { return; }
+    const { username, password, remember } = this.loginForm.value;
+    // TODO: wire auth; show a toast on success/failure
+    console.log('login', { username, password: '•••', remember });
+    this.closeLogin();
+  }
+}
