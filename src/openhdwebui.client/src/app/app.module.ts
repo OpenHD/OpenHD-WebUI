a7451707e275b23fa04653ea245c4f088912f043
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-08-23 20:59:24 +0200

    Reapply "Merge pull request #36 from OpenHD/codex/add-wifi-and-ethernet-settings-display"
    This reverts commit d587a0c770c1829441fa0542b4dda2b9ce9e169a.

--- src/openhdwebui.client/src/app/app.module.ts
+++ src/openhdwebui.client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
@@ -33,6 +33,7 @@ import { SettingsComponent } from './settings/settings.component';
         BrowserAnimationsModule,
         AppRoutingModule,
         ReactiveFormsModule,
+        FormsModule,
         MatButtonModule,
         MatCardModule,
         MatListModule
