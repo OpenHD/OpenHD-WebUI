68115900889125cda2f56b226f1c6c094fe5d689
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-08-23 20:48:29 +0200

    feat(settings): add network interface management

--- src/openhdwebui.client/src/app/settings/settings.component.spec.ts
+++ src/openhdwebui.client/src/app/settings/settings.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 import { SettingsComponent } from './settings.component';
 
@@ -8,7 +10,8 @@ describe('SettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [ SettingsComponent ],
+      imports: [HttpClientTestingModule, FormsModule]
     })
     .compileComponents();
 
--- src/openhdwebui.client/src/app/settings/settings.component.ts
+++ src/openhdwebui.client/src/app/settings/settings.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ThemeService } from '../theme.service';
+
+interface WifiInterface {
+  name: string;
+  driver: string;
+}
+
+interface EthernetInterface {
+  name: string;
+  ipAddress: string;
+  netmask: string;
+}
+
+interface NetworkInfo {
+  wifi: WifiInterface[];
+  ethernet: EthernetInterface[];
+}
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent {
-  constructor(public themeService: ThemeService) {}
+export class SettingsComponent implements OnInit {
+  network?: NetworkInfo;
+
+  constructor(public themeService: ThemeService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<NetworkInfo>('/api/network/info').subscribe(result => {
+      this.network = result;
+    }, error => console.error(error));
+  }
 
   onThemeToggle(): void {
     this.themeService.toggle();
+  }
+
+  saveEthernet(iface: EthernetInterface): void {
+    this.http.post('/api/network/ethernet', {
+      interface: iface.name,
+      ip: iface.ipAddress,
+      netmask: iface.netmask
+    }).subscribe({ error: err => console.error(err) });
   }
 }
